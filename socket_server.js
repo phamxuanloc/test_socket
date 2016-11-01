e0d4591cc5f68e8d3961da6de9f46331c3099cfd
kientn16 <ngockien16@gmail.com>
2016-11-01 17:20:27 +0700

    abv

--- socket_server.js
+++ socket_server.js
@@ -1,6 +1,7 @@
 /**
  * Created by Yamon-PC on 24-Aug-16.
  */
+var mysql    = require("mysql");
 var http     = require('http'),
 	socketIO = require('socket.io'),
 
@@ -14,9 +15,41 @@ var http     = require('http'),
 	io       = socketIO.listen(server);
 io.set('match origin protocol', true);
 // io.set('origins', '*:*');
+var con = mysql.createConnection({
+	host    : "localhost",
+	user    : "root",
+	password: "",
+	database: "cydeco"
+});
+
+con.connect(function(err) {
+	if(err) {
+		console.log('Error connecting to Db');
+		return;
+	}
+	console.log('Connection established');
+});
+
+// con.end(function(err) {
+// 	// The connection is terminated gracefully
+// 	// Ensures all previously enqueued queries are still
+// 	// before sending a COM_QUIT packet to the MySQL server.
+// });
+
 var run = function(socket) {
+	con.query('SELECT * FROM user', function(err, rows) {
+		if(err) {
+			throw err;
+		}
+
+		console.log('Data received from Db:\n');
+		for (var i = 0; i < rows.length; i++) {
+			console.log(rows[i].username);
+			socket.emit('greeting', rows[i].username);
+		}
+	});
 	// Socket process here!!!
-	socket.emit('greeting', 'Hello from Socket.IO server');
+	
 	socket.on('user-join', function(data) {
 		// console.log('User %s have joined', data);
 		socket.emit('new-user', data);
